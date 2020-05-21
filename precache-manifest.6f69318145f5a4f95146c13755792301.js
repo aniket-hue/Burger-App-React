ed9d18c8f3efe7d07a0a805c328815a8050dc904
aniket-hue <freakyyo26@gmail.com>
2020-05-21 18:15:09 +0530

    Updates

---
+++ precache-manifest.6f69318145f5a4f95146c13755792301.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e72cff329ccd4445d3f1f31a69ff4ed",
+    "url": "/aniket-hue/Burger-App-React.git/index.html"
+  },
+  {
+    "revision": "ccecf3f2f00a374182c6",
+    "url": "/aniket-hue/Burger-App-React.git/static/css/main.91968153.chunk.css"
+  },
+  {
+    "revision": "7098c1ebded7325f2aa7",
+    "url": "/aniket-hue/Burger-App-React.git/static/js/2.c7cd66a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aniket-hue/Burger-App-React.git/static/js/2.c7cd66a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccecf3f2f00a374182c6",
+    "url": "/aniket-hue/Burger-App-React.git/static/js/main.0d780913.chunk.js"
+  },
+  {
+    "revision": "d95c605b6f8b2933df22",
+    "url": "/aniket-hue/Burger-App-React.git/static/js/runtime-main.25f1530b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/aniket-hue/Burger-App-React.git/static/media/logo.b8503d26.png"
+  }
+]);
